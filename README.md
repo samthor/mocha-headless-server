bf4fc33eb3fdd99e896eba59587ed77cb7b36e6b
Sam Thorogood <sam.thorogood@gmail.com>
2018-07-18 14:21:42 +1000

    initial code

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
-# mocha-headless-server
-Headless Mocha testing on a local HTTP server
+Use to run Mocha tests via headless browsers (currently just Chrome) via a local HTTP server.
+
+Running a local HTTP server enables resources that require network access (e.g., JS module loading).
+This will serve files in the current working directory.
+
+# Usage
+
+You should have a boilerplate Mocha test file and a suite of tests.
+You'll need [Mocha](http://npmjs.com/package/mocha) and [Chai](http://npmjs.com/package/chai) added to your project.
+For a good example of the boilerplate files, check out `ok-emoji`: [harness (HTML)](https://github.com/samthor/ok-emoji/blob/master/test.html), which includes [tests (JS)](https://github.com/samthor/ok-emoji/blob/master/suite.js)
+
+## Binary
+
+From the command-line:
+
+```bash
+# either of
+yarn global add mocha-headless-server
+npm install -g mocha-headless-server
+
+# then
+mocha-headless-server path/to/test.html
+```
+
+## Node
+
+First, add this project as a dev dependency:
+
+```bash
+# either of
+npm install --save-dev mocha-headless-server
+yarn add --dev mocha-headless-server
+```
+
+Then you can `require()` this module and include it in your scripts:
+
+```js
+const tester = require('mocha-headless-server');
+
+const options = {
+  path: 'path/to/test.html',  // path, required
+  log: false,                 // whether to log to console
+  headless: true,             // whether to run headless or to show browser
+};
+const p = tester(options);
+p.then((out) => {
+  if (out.fail.length) {
+    // oh no, a test failed
+  }
+});
+```
+
+## Travis-CI
+
+First, add this project as a dev dependency:
+
+```bash
+# either of
+npm install --save-dev mocha-headless-server
+yarn add --dev mocha-headless-server
+```
+
+Then add this script to your `package.json`:
+
+```js
+{
+  "scripts": {
+    "test": "mocha-headless-server path/to/test.html"
+  }
+}
+```
+
+Add this `.travis.yml` file to your project:
+
+```yaml
+language: node_js
+dist: trusty
+node_js:
+  - "node"
+addons:
+  chrome: stable
+```
+
+And perform the usual steps to set up [Travis-CI](https://travis-ci.org/) for your project.
---
+++ index.js
@@ -0,0 +1,163 @@
+/*
+ * Copyright 2018 Sam Thorogood. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+'use strict';
+
+const puppeteer = require('puppeteer');
+const childProcess = require('child_process');
+const handler = require('serve-handler');
+const http = require('http');
+const util = require('util');
+
+function formatHost(addr) {
+  if (addr.family === 'IPv6') {
+    return `[${addr.address}]:${addr.port}`;
+  }
+  return `${addr.address}:${addr.port}`;
+}
+
+function runServer() {
+  return new Promise((resolve, reject) => {
+    const server = http.createServer(handler);  // TODO: handler serves cwd by default
+    server.listen(() => resolve(server));
+    server.on('error', reject);
+  });
+}
+
+/**
+ * Serialized and run inside the client/test page.
+ */
+function wrapMocha() {
+  const log = console.log.bind(console);
+  console.log = (...args) => {
+    if (args.length !== 0) {
+      return log(...args);
+    } else {
+      return log('');  // send blank line if no args
+    }
+  };
+
+  function runnerDone(runner) {
+    // store tests under their categories
+    const tests = {'all': []};
+    ['pass', 'fail', 'pending'].forEach((type) => {
+      const t = [];
+      tests[type] = t;
+      runner.on(type, (test) => {
+        const flat = flatten(test);
+        t.push(flat);
+        tests['all'].push(flat);
+      });
+    });
+
+    /**
+     * Flatten Mocha's `Test` object into plain JSON.
+     */
+    function flatten(test) {
+      return {
+        title: test.title,
+        duration: test.duration,
+        err: test.err ? Object.assign({}, test.err) : null,
+      };
+    }
+
+    return new Promise((resolve) => {
+      runner.on('end', () => resolve(tests));
+    });
+  }
+
+  Object.defineProperty(window, 'mocha', {
+    get() {
+      return undefined;
+    },
+    set(instance) {
+      delete window.mocha;
+      window.mocha = instance;
+
+      // trick Mocha into outputing terminal-like output
+      instance.constructor.reporters.Base.useColors = true;
+      instance.reporter(Mocha.reporters.spec);
+
+      const run = instance.run.bind(instance);
+      instance.run = (...args) => {
+        const runner = run(...args);
+
+        // steal output and log to common global to report completion
+        runnerDone(runner).then((out) => {
+          window.__mochaTest = out;
+        });
+
+        return runner;
+      };
+
+      delete window.mocha;
+      window.mocha = instance;
+    },
+    configurable: true,
+  });
+}
+
+module.exports = function({path, args=[], headless=true, log=false}) {
+  const cleanup = [];
+
+  if (process.env.CI || process.env.TRAVIS) {
+    args.push('--no-sandbox', '--disable-setuid-sandbox');
+  }
+
+  async function runner() {
+    const server = await runServer();
+    cleanup.push(() => server.close());
+
+    const addr = server.address();
+    const url = `http://${formatHost(addr)}/${path}`;
+
+    const options = {headless, args};
+    const browser = await puppeteer.launch(options);
+    cleanup.push(() => browser.close());
+
+    const pages = await browser.pages();
+    const page = pages[0] || await browser.newPage();
+
+    if (log) {
+      page.on('console', (msg) => {
+        // arg.jsonValue returns a Promise for some reason
+        const p = msg._args.map((arg) => arg.jsonValue());
+        Promise.all(p).then((args) => {
+          const out = util.format(...args);
+          process.stdout.write(out + '\n');
+        });
+      });
+      page.on('pageerror', (err) => console.error(err));
+    }
+    page.on('dialog', (dialog) => dialog.dismiss());
+
+    await page.evaluateOnNewDocument(wrapMocha);
+    await page.goto(url);
+
+    // wait for and return test result global from page
+    const timeout = 60 * 1000;
+    await page.waitForFunction(() => window.__mochaTest, {timeout});
+    return await page.evaluate(() => window.__mochaTest);
+  }
+
+  const p = runner();
+  return p.then(async () => {
+    while (cleanup.length !== 0) {
+      await cleanup.pop()();
+    }
+    return p;  // return original Promise if we cleaned up properly
+  });
+};
